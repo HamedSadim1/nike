e0d7b8a8af596cc9f1ad49c9bd90ec5dfb470358
HamedSadim1 <hamid.sadim@outlook.com>
2024-03-05 13:39:17 +0100

    Product Product is finished

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,17 +2,19 @@ import Image from "next/image";
 
 interface Props {
   label: string;
-  iconUrl: string;
+  iconUrl?: string;
 }
 const Button = ({ iconUrl, label }: Props) => {
   return (
     <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
       {label}
-      <Image
-        src={iconUrl}
-        alt="arrow-right"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+      {iconUrl && (
+        <Image
+          src={iconUrl}
+          alt="arrow-right"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
   );
 };
